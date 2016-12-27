9e2d436778e8b2c270453a874b560bbb30d06f96
dpcosta <dpcosta@gmail.com>
2016-12-27 11:27:59 -0200

    Usando a mesma view em actions diferentes. Passando dados para a view através da ViewBag,

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace Blog.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.Linguagem = "ASP.NET MVC";
             return View();
         }
 
@@ -24,6 +25,20 @@ namespace Blog.Controllers
         public ActionResult Categorias()
         {
             return View();
+        }
+
+        //GET: Home/OlaMundoCSharp
+        public ActionResult OlaMundoCSharp()
+        {
+            ViewBag.Linguagem = "C#";
+            return View("Index");
+        }
+
+        //GET: Home/OlaMundoJavascript
+        public ActionResult OlaMundoJavascript()
+        {
+            ViewBag.Linguagem = "Javascript";
+            return View("Index");
         }
     }
 }
