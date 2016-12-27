59322be6225b481c2de49bb2dc191a1ec535d3e1
dpcosta <dpcosta@gmail.com>
2016-12-27 13:49:32 -0200

    Nova funcionalidade: inclusão de um post.

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -16,7 +16,7 @@ namespace Blog.Controllers
         {
             var listaDePosts = new List<Post>();
 
-            using (SqlConnection cnx = new SqlConnection("connection string"))
+            using (SqlConnection cnx = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Blog.mdf;Integrated Security=True"))
             {
                 cnx.Open();
                 SqlCommand selectCmd = cnx.CreateCommand();
@@ -24,17 +24,42 @@ namespace Blog.Controllers
 
                 SqlDataReader resultado = selectCmd.ExecuteReader();
                 while (resultado.Read())
-                {
-                    new Post() {
+                { 
+                    listaDePosts.Add(new Post()
+                    {
                         Titulo = Convert.ToString(resultado["titulo"]),
                         Resumo = Convert.ToString(resultado["resumo"]),
                         Categoria = Convert.ToString(resultado["categoria"])
-                    };
+                    });
                 };
 
             }
 
             ViewBag.Posts = listaDePosts;
+            return View();
+        }
+
+        public ActionResult Novo()
+        {
+            return View();
+        }
+
+        public ActionResult Adiciona(Post p)
+        {
+            using (SqlConnection cnx = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Blog.mdf;Integrated Security=True"))
+            {
+                cnx.Open();
+                SqlCommand insertCmd = cnx.CreateCommand();
+                insertCmd.CommandText = "insert into Post (Titulo, Resumo, Categoria) values (@Titulo, @Resumo, @Categoria)";
+
+                insertCmd.Parameters.Add(new SqlParameter("Titulo", p.Titulo));
+                insertCmd.Parameters.Add(new SqlParameter("Resumo", p.Resumo));
+                insertCmd.Parameters.Add(new SqlParameter("Categoria", p.Categoria));
+
+                insertCmd.ExecuteNonQuery();
+
+            }
+            ViewBag.Titulo = p.Titulo;
             return View();
         }
     }
